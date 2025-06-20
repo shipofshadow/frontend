432e253b8f1f91db606fe7983a0150d4c7c26958
bitress <cyannejustinl.vega12@gmail.com>
2025-06-20 20:10:33 +0800

    moved css to other repo

--- src/pages/applicant/Apply.tsx
+++ src/pages/applicant/Apply.tsx
@@ -5,10 +5,12 @@ import CampusSelect from "../../components/selects/CampusSelect.tsx";
 import CourseSelect from "../../components/selects/CourseSelect.tsx";
  import { useAcademicTerm } from "../../hooks/useAcademicTerm.ts";
 import type {ApplicationForm} from "../../interfaces/ApplicationForm.ts";
+import {useAuth} from "../../context/AuthContext.tsx";
 
 const Apply = () => {
-  const [step, setStep] = useState("step2");
+  const [step, setStep] = useState("step1");
   const { term } = useAcademicTerm();
+  const { user } = useAuth();
 
   const [formData, setFormData] = useState<ApplicationForm>({
     // Applicant Details
@@ -32,7 +34,7 @@ const Apply = () => {
     academicAchievements: "",
 
     // Scholarship Information
-    scholarshipType: "",
+    scholarshipName: "",
     financialNeed: "",
     careerGoals: "",
 
@@ -334,21 +336,21 @@ const Apply = () => {
                       </div>
 
                       <div className="mb-3 col-md-4">
-                        <label>Maiden Name</label>
+                        <label>Mother's Maiden Last Name</label>
                         <input className="form-control" type="text"
                                value={formData.mother_last_name}
                                onChange={(e) => handleInputChange('mother_last_name', e.target.value)}
                         />
                       </div>
                       <div className="mb-3 col-md-4">
-                        <label>First Name</label>
+                        <label>Mother's First Name</label>
                         <input className="form-control" type="text"
                                value={formData.mother_first_name}
                                onChange={(e) => handleInputChange('mother_first_name', e.target.value)}
                         />
                       </div>
                       <div className="mb-3 col-md-4">
-                        <label>Middle Name</label>
+                        <label>Mother's Maiden Middle Name</label>
                         <input className="form-control" type="text"
                                value={formData.mother_middle_name}
                                onChange={(e) => handleInputChange('mother_middle_name', e.target.value)}
@@ -369,6 +371,29 @@ const Apply = () => {
                                onChange={(e) => handleInputChange('mother_income', e.target.value)}
                         />
                       </div>
+
+
+
+                      <div className="col-12">
+                        <h5>Emergency Information</h5>
+                      </div>
+
+                      <div className="mb-3 col-md-6">
+                        <label>Emergency Contact Name</label>
+                        <input className="form-control" type="number"
+                               value={formData.siblings_studying}
+                               onChange={(e) => handleInputChange('siblings_studying', e.target.value)}
+                        />
+                      </div>
+
+                      <div className="mb-3 col-md-6">
+                        <label>Emergency Contact Number</label>
+                        <input className="form-control" type="number"
+                               value={formData.siblings_studying}
+                               onChange={(e) => handleInputChange('siblings_studying', e.target.value)}
+                        />
+                      </div>
+
 
                       {/* Household Info */}
                       <div className="col-12">
@@ -404,17 +429,20 @@ const Apply = () => {
                         />
                       </div>
                       <div className="mb-3 col-md-6">
-                        <label>4Ps Member</label>
+                        <label>Are you a recipient of any DSWD Program?</label>
                         <select className="form-control"
                                 value={formData.is_4ps_member}
                                 onChange={(e) => handleInputChange('is_4ps_member', e.target.value)}
                         >
-                          <option value="">Select</option>
-                          <option value="1">Yes</option>
-                          <option value="0">No</option>
+                          <option disabled selected>Select DSWD Program</option>
+                          <option value="Listahan">Listahan</option>
+                          <option value="4Ps">4Ps</option>
                         </select>
                       </div>
+
+
                     </div>
+
 
                     <div className="d-flex justify-content-between mt-4">
                       <button className="btn btn-light disabled" disabled>Previous</button>
@@ -430,6 +458,12 @@ const Apply = () => {
                   <h5 className="card-title mb-4">Educational Background and Achievements</h5>
                   <div>
                     <div className="row gx-3">
+
+                      <div className="mb-3 col-md-4">
+                        <label htmlFor="student_id" className="form-label">Student ID</label>
+                        <input type="text" name="student_id" value={user.profile.student_id} className="form-control"/>
+                      </div>
+
                       <div className="mb-3 col-md-4">
                         <label htmlFor="campus" className="form-label">Campus</label>
                         <CampusSelect
@@ -473,7 +507,7 @@ const Apply = () => {
                     <input type="hidden" name="semester_id" value={term?.semester_id} />
 
                     <div className="mb-3 col-md-4">
-                      <label className="form-label">Total Units</label>
+                      <label className="form-label">Total Units Enrolled</label>
                         <input type="number" className="form-control" placeholder="Enter the total units"/>
                       </div>
 
@@ -494,34 +528,83 @@ const Apply = () => {
 
                     <div className="row mb-3">
                       <div className="col-md-4">
-                        <label htmlFor="scholarshipId" className="form-label">Main Scholarship</label>
+                        <label htmlFor="scholarshipName" className="form-label">Scholarship Name</label>
                         <select
-                            id="scholarshipId"
-                            name="scholarshipId"
+                            id="scholarshipName"
+                            name="scholarshipName"
                             className="form-select"
-                            value={formData.scholarshipId}
-                            onChange={(e) => handleInputChange('scholarshipId', e.target.value)}
+                            value={formData.scholarshipName}
+                            onChange={(e) => handleInputChange('scholarshipName', e.target.value)}
                             required
                         >
-                          <option value="">Select Scholarship</option>
-                          <option value="1">iScholar Grant</option>
-                          <option value="2">Merit Scholarship</option>
-                          {/* You can dynamically map from a scholarships array */}
+                          <option selected disabled>Select Scholarship</option>
+                          <option value="NONE">NONE</option>
+                          <option value="ATI-CAR EDUCATIONAL ASSISTANCE FOR THE YOUTH IN AGRICULTURE (EASY-AGRI)">ATI-CAR EDUCATIONAL ASSISTANCE FOR THE YOUTH IN AGRICULTURE (EASY-AGRI)</option>
+                          <option value="ANSWERING THE CRY OF THE P0OR (ANCOP)">ANSWERING THE CRY OF THE P0OR (ANCOP)</option>
+                          <option value="ASA PHILIPPINES">ASA PHILIPPINES</option>
+                          <option value="BACNOTAN COMPREHENSIVE EDUCATIONAL ASSISTANCE PROGRAM">BACNOTAN COMPREHENSIVE EDUCATIONAL ASSISTANCE PROGRAM </option>
+                          <option value="BUREAU OF FISHERIES AND AQUATIC RESOURCES">BUREAU OF FISHERIES AND AQUATIC RESOURCES</option>
+                          <option value="CANDON CITY SCHOLARSHIP">CANDON CITY SCHOLARSHIP</option>
+                          <option value="CANDONIANS OF SOUTHERN CALIFORNIA">CANDONIANS OF SOUTHERN CALIFORNIA</option>
+                          <option value="CARITAS NUEVA SEGOVIA">CARITAS NUEVA SEGOVIA</option>
+                          <option value="CERVANTES EDUCATIONAL ASSISTANCE PROGRAM">CERVANTES EDUCATIONAL ASSISTANCE PROGRAM</option>
+                          <option value="COLLEGE EDUCATIONAL ASSISTANCE PROGRAM (CEAP)">COLLEGE EDUCATIONAL ASSISTANCE PROGRAM (CEAP)</option>
+                          <option value="CONGRESMAN ERIC D. SINGSON SCHOLARSHIP GRANT (CEDSSG)">CONGRESMAN ERIC D. SINGSON SCHOLARSHIP GRANT (CEDSSG)</option>
+                          <option value="CHAVIT SINGSON SCHOLARSHIP">CHAVIT SINGSON SCHOLARSHIP</option>
+                          <option value="CHED-FULL">CHED-FULL</option>
+                          <option value="CHED-HALF">CHED-HALF</option>
+                          <option value="CHED-SMART">CHED-SMART</option>
+                          <option value="CHED-TULONG AGRI PROGRAM (CHED-TAP)">CHED-TULONG AGRI PROGRAM (CHED-TAP)</option>
+                          <option value="CHED-TULONG DUNONG PROGRAM (CHED-TDP)">CHED-TULONG DUNONG PROGRAM (CHED-TDP)</option>
+                          <option value="CHED-TERTIARY EDUCATION SUBSIDY (CHED-TES)">CHED-TERTIARY EDUCATION SUBSIDY (CHED-TES)</option>
+                          <option value="CITIZEN'S BATTLE AGAINST CORRUPTION (CIBAC) SCHOLARSHIP">CITIZEN'S BATTLE AGAINST CORRUPTION (CIBAC) SCHOLARSHIP</option>
+                          <option value="DA-AGRICULTURAL COMPETITIVENESS ENHANCEMENT FUND (DA-ACEF)">DA-AGRICULTURAL COMPETITIVENESS ENHANCEMENT FUND (DA-ACEF)</option>
+                          <option value="DEPARTMENT OF HEALTH SCHOLARSHIP">DEPARTMENT OF HEALTH SCHOLARSHIP</option>
+                          <option value="DEPARTMENT OF SCIENCE AND TECHNOLOGY (DOST)">DEPARTMENT OF SCIENCE AND TECHNOLOGY (DOST)</option>
+                          <option value="DEPARTMENT OF SOCIAL WELFARE AND DEVELOPMENT (DSWD)">DEPARTMENT OF SOCIAL WELFARE AND DEVELOPMENT (DSWD)</option>
+                          <option value="EDUCATIONAL ASSISTANCE AND SCHOLARSHIP EMERGENCIES (EASE-AGRI)">EDUCATIONAL ASSISTANCE AND SCHOLARSHIP EMERGENCIES (EASE-AGRI)</option>
+                          <option value="EVA AND EDUARDSON FOUNDATION INC.">EVA AND EDUARDSON FOUNDATION INC.</option>
+                          <option value="ILOCOS SUR AGRICULTURE COLLEGE BATCH 1971 (ISAC BATCH'71)">ILOCOS SUR AGRICULTURE COLLEGE BATCH 1971 (ISAC BATCH'71)</option>
+                          <option value="ILOCOS SUR EDUCATIONAL ASSISTANCE AND SCHOLARSHIP PROGRAM (ISEASP)">ILOCOS SUR EDUCATIONAL ASSISTANCE AND SCHOLARSHIP PROGRAM (ISEASP)</option>
+                          <option value="ILOCOS SUR ELECTRIC COOPERATIVE (ISECO)">ILOCOS SUR ELECTRIC COOPERATIVE (ISECO)</option>
+                          <option value="LA UNION EDUCATIONAL ASSISTANCE">LA UNION EDUCATIONAL ASSISTANCE</option>
+                          <option value="LEPANTO EDUCATIONAL ASSISTANCE PROGRAM (LEAP)">LEPANTO EDUCATIONAL ASSISTANCE PROGRAM (LEAP)</option>
+                          <option value="MANILA TEACHERS MUTUAL AIDE SYSTEM (MTMAS)">MANILA TEACHERS MUTUAL AIDE SYSTEM (MTMAS)</option>
+                          <option value="MSJAB SCHOLARSHIP">MSJAB SCHOLARSHIP</option>
+                          <option value="MUNICIPAL SCHOLARS">MUNICIPAL SCHOLARS</option>
+                          <option value="NATIONAL COMMISSION IN INDIGENOUS PEOPLE (NCIP)">NATIONAL COMMISSION IN INDIGENOUS PEOPLE (NCIP)</option>
+                          <option value="NATIONAL TOBACCO ADMINISTRATION (NTA)">NATIONAL TOBACCO ADMINISTRATION (NTA)</option>
+                          <option value="ONE TIME EDUC ATIONAL ASSISTANCE PROGRAM (OTAP)">ONE TIME EDUCATIONAL ASSISTANCE PROGRAM (OTAP)</option>
+                          <option value="OVERSEAS WORKERS WELFARE ADMINISTRATION (OWWA)">OVERSEAS WORKERS WELFARE ADMINISTRATION (OWWA)</option>
+                          <option value="PHILIPPINE CHARITY SWEEPSTAKE OFFICE STUDENT PROGRAM">PHILIPPINE CHARITY SWEEPSTAKE OFFICE STUDENT PROGRAM</option>
+                          <option value="PRIVATE/INDIVIDUAL SCHOLARSHIP">PRIVATE/INDIVIDUAL SCHOLARSHIP</option>
+                          <option value="PROVINCIAL GOVERNMENT OF LA UNION (PGLU)">PROVINCIAL GOVERNMENT OF LA UNION (PGLU)</option>
+                          <option value="RANIAG SCHOLARSHIP PROGRAM">RANIAG SCHOLARSHIP PROGRAM</option>
+                          <option value="SCHOLARSHIP PROGRAM FOR COCONUT FARMERS AND FAMILIES (CoScho)">SCHOLARSHIP PROGRAM FOR COCONUT FARMERS AND FAMILIES (CoScho)</option>
+                          <option value="SONS AND DAUGHTERS OF NAGBUKEL">SONS AND DAUGHTERS OF NAGBUKEL</option>
+                          <option value="TECHNICAL EDUCATIONAL SKILL DEVELOPMENT AUTHORITY (TESDA)">TECHNICAL EDUCATIONAL SKILL DEVELOPMENT AUTHORITY (TESDA)</option>
+                          <option value="UNIVERSAL LEAF PHILIPPINES INC. (ULPI)">UNIVERSAL LEAF PHILIPPINES INC. (ULPI)</option>
+                          <option value="U-GO SCHOLARSHIP">U-GO SCHOLARSHIP</option>
+                          <option value="VIGAN CITY SCHOLARSHIP">VIGAN CITY SCHOLARSHIP</option>
+                          <option value="OTHERS">OTHERS</option>
                         </select>
                       </div>
 
-                      <div className="col-md-4">
-                        <label htmlFor="otherScholarship" className="form-label">Other Scholarship(s)</label>
-                        <input
-                            type="text"
-                            id="otherScholarship"
-                            name="otherScholarship"
-                            className="form-control"
-                            placeholder="e.g., DOST, CHED"
-                            value={formData.otherScholarship}
-                            onChange={(e) => handleInputChange('otherScholarship', e.target.value)}
-                        />
-                      </div>
+                      {formData.scholarshipName === 'OTHERS' && (
+                          <div className="col-md-4">
+                            <label htmlFor="otherScholarship" className="form-label">Other Scholarship(s)</label>
+                            <input
+                                type="text"
+                                id="otherScholarship"
+                                name="otherScholarship"
+                                className="form-control"
+                                placeholder="e.g., DOST, CHED"
+                                value={formData.otherScholarship}
+                                onChange={(e) => handleInputChange('otherScholarship', e.target.value)}
+                            />
+                          </div>
+                      )}
+
 
                       <div className="col-md-4">
                         <label htmlFor="otherScholarshipAmount" className="form-label">Amount (₱)</label>
