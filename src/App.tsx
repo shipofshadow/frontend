3f7078e2e723b9e9d586db9d0b52f5b10f4c2f14
bitress <cyannejustinl.vega12@gmail.com>
2025-07-09 17:23:31 +0800

    gfix

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import AppRoutes from './routes/AppRoutes';
-
+import ApiStatusGuard from "./components/ApiStatusGuard.tsx";
 
 const App: React.FC = () => {
-  return (
-    <Router>
-<<<<<<< HEAD
-        
-            <AppRoutes />
-       
-=======
-        <AppRoutes />
->>>>>>> bdd07267b184537706a1db265b2763e9aacea33a
-    </Router>
-  );
+    return (
+        <Router>
+            <ApiStatusGuard>
+                <AppRoutes />
+            </ApiStatusGuard>
+        </Router>
+    );
 };
 
 export default App;
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -39,7 +39,6 @@ import CampusReport from "../pages/admin/reports/CampusReport.tsx";
 import ApplicantsReport from "../pages/admin/reports/ApplicantsReport.tsx";
 import ArchivedDocuments from "../pages/admin/documents/ArchivedDocuments.tsx";
 import SubmittedRequirements from "../pages/admin/documents/SubmittedRequirements.tsx";
-import {Import} from "lucide-react";
 import FuzzyLogic from "../pages/admin/FuzzyLogic.tsx";
 import ImportStudents from "../pages/admin/ImportStudents.tsx";
 
@@ -62,25 +61,7 @@ const AppRoutes: React.FC = () => {
         <Routes>
             <Route path="/" element={isAuthenticated ? <Navigate to="/applicant" replace /> : <App />} />
 
-<<<<<<< HEAD
-            <Route path="/admin" element={<AdminLayout />}>
-                <Route index element={<Dashboard />} />
-                <Route path="manage-students" element={<ManageStudents />} />
-                <Route path="Applicants" element={<Applicants />} />
-                <Route path="users" element={<Users />} />
-                <Route path="scholarship-management" element={<ScholarshipManagement />} />
-                <Route path="config-system" element={<SystemSetting />} />
-                <Route path="documents" element={<Document />} />
-                <Route path="activity-logs" element={<ActivityLogs />} />
-                <Route path="archived-applicants" element={<ArchivedApplicants />} />
-                <Route path="reports" element={<Reports />} />
-                <Route path="campuses" element={<Campuses />} />
-                <Route path="bulk-evaluation" element={<BulkEvaluation />} />
-                <Route path="metrics" element={<Metrics />} />
-                <Route path="departments" element={<Departments />} />
-                <Route path="academic-years" element={<AcademicYears />} />
-                <Route path="courses" element={<Courses />} />
-=======
+
             <Route element={<ProtectedRoute allowedRoles={["admin"]} />}>
                 <Route path="/admin" element={<AdminLayout />}>
                     <Route index element={<Dashboard />} />
@@ -125,7 +106,6 @@ const AppRoutes: React.FC = () => {
                     <Route path="fuzzy-logic" element={<FuzzyLogic />} />
 
                 </Route>
->>>>>>> bdd07267b184537706a1db265b2763e9aacea33a
             </Route>
 
 
