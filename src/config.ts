24c1f38d185bd2c6de2d11d996a60fa4fce5e12e
byteress <byteress@gmail.com>
2026-09-21 18:30:35 +0800

    ff

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = (import.meta as any).env.VITE_API_BASE_URL || 'http://localhost:8000';
+export const API_BASE_URL = (import.meta as any).env.VITE_API_BASE_URL || 'https://api.ischolar.space';
